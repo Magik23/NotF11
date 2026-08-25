d86adbbcae7399b98547e65b3e6a39035d5ef20f
Pierre Dionne <99895436+Magik23@users.noreply.github.com>
2026-08-25 17:55:15 -0600

    feat: handle extension action clicks

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
 console.log("Chromium Clean Window service worker started.");
+
+chrome.action.onClicked.addListener((tab) => {
+  console.log("Extension action clicked.");
+  console.log("Active tab ID:", tab.id);
+  console.log("Active window ID:", tab.windowId);
+});
